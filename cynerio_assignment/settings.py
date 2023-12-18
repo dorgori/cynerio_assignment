7bf9507e859c5c4379fb4ee1e64a88f17fd01a2f
dorgori7@gmail.com <dorgori7@gmail.com>
2023-12-18 14:49:46 +0200

    cleanups

--- cynerio_assignment/settings.py
+++ cynerio_assignment/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-0e-hw#b=j-rg=0g!f!%%v$kuu)g0jj%enh2-4j_b6rzg@dbrqs'
+SECRET_KEY = 'django-insecure-0e-hw#b=j-rg=0g!f!%%v$kuu)g0jj%enh2-4j_b6rzg@dbrqs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
